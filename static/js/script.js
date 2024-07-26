0b445385dd2ec5b195052eedd1471afc60019c80
Felipe-Tamura <felipetamura59@gmail.com.br>
2024-07-25 23:11:25 -0300

    Trocando simbolo do botão dinamicamente

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,25 @@
-const tecladoNumerico = document.querySelector('.teclado_numerico')
-const tecladoAcao = document.querySelector('.teclado_acao')
-const tecladoLimpar = document.querySelector('.teclado_limpar')
-const telaConsole = document.querySelector('.tela_calc')
-const telaOperacao = document.querySelector('.tela_simbolo')
-const historico = document.querySelector('.tabela_historico')
-const tabelaHistorico = document.querySelector('.linhas_historico')
-const btnHistorico = document.querySelector('.btn_historico')
-let operacoes = []
-let verHistorico = false
+const tecladoNumerico = document.querySelector(".teclado_numerico");
+const tecladoAcao = document.querySelector(".teclado_acao");
+const tecladoLimpar = document.querySelector(".teclado_limpar");
+const telaConsole = document.querySelector(".tela_calc");
+const telaOperacao = document.querySelector(".tela_simbolo");
+const historico = document.querySelector(".tabela_historico");
+const tabelaHistorico = document.querySelector(".linhas_historico");
+const btnHistorico = document.querySelector(".btn_historico");
+let operacoes = [];
+let verHistorico = false;
 
 function start() {
     /*
      * Inicializando calculadora
      */
 
-    criarTeclas() //Criando teclas
+    criarTeclas(); //Criando teclas
 
     //Visibilidade do histórico
-    btnHistorico.addEventListener('click', () => {
-        historico.classList.toggle('escondido')
-    })
+    btnHistorico.addEventListener("click", () => {
+        historico.classList.toggle("escondido");
+    });
 }
 
 function criarTeclas() {
@@ -28,16 +28,15 @@ function criarTeclas() {
      * teclas numéricas, de ação e de limpeza do console
      */
 
-    var linha = 4
-    var coluna = 3
+    var linha = 4;
+    var coluna = 3;
 
-    const teclasAcao = ["/", "*", "-", "+"]
-    const teclasLimpar = ["CE", "C", "<"]
+    const teclasAcao = ["/", "*", "-", "+"];
+    const teclasLimpar = ["CE", "C", "&#8656;"];
 
     // Criando teclas numéricas
     for (let i = 1; i <= linha * coluna; i++) {
-
-        let temporario = i == 10 ? "." : (i == 12 ? "," : (i == 11 ? 0 : i))// Variável temporaria para guardar informações
+        let temporario = i == 10 ? "." : i == 12 ? "," : i == 11 ? 0 : i; // Variável temporaria para guardar informações
         tecladoNumerico.innerHTML += `<button
                 id="botao_numerico"
                 class="tecla"
@@ -45,131 +44,145 @@ function criarTeclas() {
                 onclick="getValue(this.value)"
                 value="${temporario}">
                     ${temporario}
-                </button>`
+                </button>`;
     }
 
     // Criando teclas de ação (-, +, *, ...)
-    teclasAcao.forEach(tecla => {
-        let simbolo = document.createElement('button') //Criando um botão
-        simbolo.type = "button" //Definindo tipo do botão
-        simbolo.className = "tecla" //Nome da classe
-        simbolo.textContent = tecla //Conteudo do botão
-        simbolo.value = tecla //Valor do botão
-        simbolo.onclick = () => { getSymbol(simbolo.value) } //Ação do botão
-        tecladoAcao.appendChild(simbolo) //Adicionando o botão no HTML
-    })
+    teclasAcao.forEach((tecla) => {
+        let simbolo = document.createElement("button"); //Criando um botão
+        simbolo.type = "button"; //Definindo tipo do botão
+        simbolo.className = "tecla"; //Nome da classe
+        simbolo.textContent = tecla; //Conteudo do botão
+        simbolo.value = tecla; //Valor do botão
+        simbolo.onclick = () => {
+            getSymbol(simbolo.value);
+        }; //Ação do botão
+        tecladoAcao.appendChild(simbolo); //Adicionando o botão no HTML
+    });
 
     // Criando teclas de limpeza do console(CE, C, backspace)
-    teclasLimpar.forEach(tecla => {
-        let simbolo = document.createElement('button') //Criando um botão
-        simbolo.type = "button" //Definindo o tipo do botão
-        simbolo.className = "tecla" //Nome da classe
-        simbolo.textContent = tecla //Conteudo do botão
-        simbolo.value = tecla //Valor do botão
-        simbolo.onclick = () => { limpar(simbolo.value) } //Ação do botão
-        tecladoLimpar.appendChild(simbolo) //Adicionando o botão no HTML
-    })
+    teclasLimpar.forEach((tecla) => {
+        let simbolo = document.createElement("button"); //Criando um botão
+        simbolo.type = "button"; //Definindo o tipo do botão
+        simbolo.className = "tecla"; //Nome da classe
+        if (tecla === "&#8656;") {
+            simbolo.innerHTML = tecla;
+            simbolo.value = "backspace";
+        } else {
+            simbolo.textContent = tecla; //Conteudo do botão
+            simbolo.value = tecla; //Valor do botão
+        }
+        simbolo.onclick = () => {
+            limpar(simbolo.value);
+        }; //Ação do botão
+        tecladoLimpar.appendChild(simbolo); //Adicionando o botão no HTML
+    });
 
     // Criando tecla para calcular
-    let teclaCalcular = document.createElement('button')
-    teclaCalcular.type = 'button'
-    teclaCalcular.className = 'tecla'
-    teclaCalcular.textContent = '='
-    teclaCalcular.onclick = () => { calcular() }
-    tecladoAcao.appendChild(teclaCalcular)
+    let teclaCalcular = document.createElement("button");
+    teclaCalcular.type = "button";
+    teclaCalcular.className = "tecla";
+    teclaCalcular.textContent = "=";
+    teclaCalcular.onclick = () => {
+        calcular();
+    };
+    tecladoAcao.appendChild(teclaCalcular);
 }
 
-function limpar(value){
+function limpar(value) {
     /*
      * Limpando a tela do console e de operações com base no parâmetro
      * Args:
      *  - value: Valor para verificar o tipo de limpeza
-     */ 
+     */
 
-    if (value == "C"){// Limpando as duas telas
-        telaConsole.innerText = ""
-        telaOperacao.innerText = ""
-    }else if (value == "CE"){// Limpando apenas o console
-        telaConsole.innerText = ""
-    }else if(value == "<"){// Backspace
-        let temp = telaConsole.innerText
-        if (temp.length > 0){
-            telaConsole.innerText = temp.slice(0, -1)
+    if (value == "C") {
+        // Limpando as duas telas
+        telaConsole.innerText = "";
+        telaOperacao.innerText = "";
+    } else if (value == "CE") {
+        // Limpando apenas o console
+        telaConsole.innerText = "";
+    } else if (value == "backspace") {
+        // Backspace
+        let temp = telaConsole.innerText;
+        if (temp.length > 0) {
+            telaConsole.innerText = temp.slice(0, -1);
         }
     }
 }
 
-function calcular(){
+function calcular() {
     /*
      * Calculando os valores da calculadora, caso ocorra erro ele mostra no console o erro
      */
 
     try {
         // Calculando operações
-        telaOperacao.innerText += telaConsole.innerText
-        let resultado = eval(telaOperacao.innerText)
+        telaOperacao.innerText += telaConsole.innerText;
+        let resultado = eval(telaOperacao.innerText);
 
-        if (resultado == "Infinity"){// Verificando se há divisão por zero
+        if (resultado == "Infinity") {
+            // Verificando se há divisão por zero
 
-            alert("Divisão por zero não é válido!")
-            limpar("C")
-
-        }else{
-
+            alert("Divisão por zero não é válido!");
+            limpar("C");
+        } else {
             // Adicionando dados a lista de operações
-            operacoes.push({equacao: telaOperacao.innerText, resultado: resultado})
+            operacoes.push({ equacao: telaOperacao.innerText, resultado: resultado });
 
             // Limpando as telas e mostrando resultado na tela
-            limpar("C")
-            telaConsole.innerText = resultado
+            limpar("C");
+            telaConsole.innerText = resultado;
 
             // Atualizando historico de operacoes
-            generateTable()
+            generateTable();
         }
     } catch (error) {
-        console.error("Erro ao calcular: ", error)
+        console.error("Erro ao calcular: ", error);
     }
 }
 
-function getValue(value){
+function getValue(value) {
     /*
      * Pegando o valor do botão e adicionando no console
      */
 
-    telaConsole.innerText += value
+    telaConsole.innerText += value;
 }
 
-function getSymbol(value){
+function getSymbol(value) {
     /*
      * Pegando simbolos de equações e adicionando na tela de operaçoes
      * junto com valores do console
      */
 
-    telaOperacao.innerText = telaConsole.innerText
-    telaOperacao.innerText += value
-    telaConsole.innerText = ''
+    telaOperacao.innerText = telaConsole.innerText;
+    telaOperacao.innerText += value;
+    telaConsole.innerText = "";
 }
 
-function generateTable(){
+function generateTable() {
     /*
      * Adicionando valores na tabela caso exista alguma operação na lista
      */
 
     // Limpando tabela de histórico
-    tabelaHistorico.innerHTML = ''
+    tabelaHistorico.innerHTML = "";
 
-    if(operacoes.length > 0){// Adicionando valores
+    if (operacoes.length > 0) {
+        // Adicionando valores
 
         // Iterando sobre elementos
         operacoes.forEach((dados) => {
-            let row = tabelaHistorico.insertRow() // Criando linhas
-            let equacaoCell = row.insertCell(0) // Criando linhas da equação
-            let resultadoCell = row.insertCell(1) // Criando linhas do resultado
+            let row = tabelaHistorico.insertRow(); // Criando linhas
+            let equacaoCell = row.insertCell(0); // Criando linhas da equação
+            let resultadoCell = row.insertCell(1); // Criando linhas do resultado
 
-            equacaoCell.innerText = dados.equacao // Adicionando equação a tabela
-            resultadoCell.innerText = dados.resultado // Adicionando resultado a tabela
-        })
+            equacaoCell.innerText = dados.equacao; // Adicionando equação a tabela
+            resultadoCell.innerText = dados.resultado; // Adicionando resultado a tabela
+        });
     }
 }
 
-start()
+start();
